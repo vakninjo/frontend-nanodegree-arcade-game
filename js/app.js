452c94aa691d93555cae6f6fe76a87d87a746011
Jonathan Waknin <vakninjo@gmail.com>
2018-08-26 16:02:30 -0500

    Hero render with basic movement - include pseudo code

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
+    //x position
+    //y position
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    //if enemy is no passed boundary
+      //move forward
+      // increment x by speed * dt
+    //else
+      //reset pos to start
 };
 
 // Draw the enemy on the screen, required method for game
@@ -26,9 +35,85 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 
+//hero class
+  //constructor
+    //properties
+        //xpixel
+        //ypixel
+        //startX
+        //startY
+        //x position
+        //y position
+        //sprite imnage
+    //methods
+      //update
+            //check for collision
+              //did play x and y collide with enemy?
+            //check fo win?
+              //did the playr x and y reach final tile?
+      //render
+          //draw player sprite on current position
+      // handle keybord input
+          //update player according to input
+      //reset hero
+        //set x and y to starting position
+
+class Hero {
+  constructor(){
+    this.sprite = 'images/char-boy.png';
+    this.step = 101;
+    this.jump = 83;
+    this.startX = this.step * 2;
+    this.startY = (this.jump * 5) - 25;
+    this.x = this.startX;
+    this.y = this.startY;
+  }
+  //draw hero
+  render() {
+    ctx.drawImage(Resources.get(this.sprite),this.x, this.y);
+  }
+
+  /**
+  *  Hero position
+  *  @param {string} input - hero direction
+  */
+  handleInput(input){
+    switch(input) {
+      case 'left':
+        if (this.x > 0) {
+          this.x -= this.step;
+        }
+        break;
+      case 'right':
+        if (this.x < this.step * 4){
+          this.x += this.step;
+        }
+        break;
+      case 'up':
+        if (this.y > this.jump ){
+          this.y -= this.jump;
+        }
+        break;
+      case 'down':
+        if (this.y < this.jump * 4){
+          this.y += this.jump;
+        }
+        break;
+    }
+  }
+}
+
+const player = new Hero();
+
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+  //init allEnemies array
+  //For each enemy create and push new Enemy objet into above array
+  //
 
 
 
